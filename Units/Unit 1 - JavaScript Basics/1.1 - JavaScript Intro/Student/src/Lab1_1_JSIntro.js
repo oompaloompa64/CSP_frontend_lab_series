d6d5595b0cd21da338e07068bc3c695d7becef6e
oompaloompa64 <nflores28150@columbushs.com>
2025-10-21 20:40:45 -0400

    finished the work

--- Units/Unit 1 - JavaScript Basics/1.1 - JavaScript Intro/Student/src/Lab1_1_JSIntro.js
+++ Units/Unit 1 - JavaScript Basics/1.1 - JavaScript Intro/Student/src/Lab1_1_JSIntro.js
@@ -7,16 +7,27 @@
 console.log("Nico F");
 
 // TODO 2: Declare variable favoriteFood, assign "Steak", then reassign "Pizza"
-
+var favoriteFood = "Steak";
+console.log (favoriteFood);
+var favoriteFood = "Pizza";
+console.log (favoriteFood);
 
 // TODO 3: Create variable mystery, assign a string, then a number, print type each time
-
+var mystery = "Hello";
+console.log(typeof mystery);
+var mystery = 4167;
+console.log(typeof mystery);
 
 // TODO 4: Declare const schoolName, try to reassign
-
+const schoolname = "cchs";
 
 // TODO 5: Create three valid camelCase variables and print them
-
+var morningRoutineChoice = "Jogging";
+var afternoonStudyTopic = "Algorithms";
+var eveningRelaxationPlan = "Reading";
+console.log(morningRoutineChoice);
+console.log(afternoonStudyTopic);
+console.log(eveningRelaxationPlan);
 
 // 🚀 Stretch Goals (optional)
 // - Create a variable age and print "I am X years old" using concatenation
@@ -25,7 +36,11 @@ console.log("Nico F");
 /* ===========================================================
    🎟 EXIT TICKET
    1. What new concept or skill did you learn in this lab?
+   assigning constants and variables
    2. How did you apply logic or conditionals in your code?
+   i didnt
    3. What challenges did you encounter, and how did you solve them?
+   i didnt know what camel case was so i saked the ai what it was
    4. Where else could this programming concept be useful?
+   constants and vars can be used for differentpourposes
    =========================================================== */
