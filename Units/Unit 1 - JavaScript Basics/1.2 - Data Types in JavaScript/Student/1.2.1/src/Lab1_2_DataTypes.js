13c91c562ba1f6cca1a36ba4adf34699f3ab6d6d
oompaloompa64 <nflores28150@columbushs.com>
2025-11-04 10:13:38 -0500

    went over 1.2.1

--- Units/Unit 1 - JavaScript Basics/1.2 - Data Types in JavaScript/Student/1.2.1/src/Lab1_2_DataTypes.js
+++ Units/Unit 1 - JavaScript Basics/1.2 - Data Types in JavaScript/Student/1.2.1/src/Lab1_2_DataTypes.js
@@ -2,14 +2,14 @@
  Lab 002 – Data Types in JavaScript
 */
 
-const age = 34;
-const price = 12.99;
+let age = 34;
+let price = 12.99;
 console.log(age, price);
-const sandraDivision = "Sandra" / 2;
+let sandraDivision = "Sandra" / 2;
 console.log(sandraDivision);
 
-const firstNumber = 10;
-const secondNumber = 3;
+let firstNumber = 10;
+let secondNumber = 3;
 console.log(firstNumber + secondNumber);
 console.log(firstNumber - secondNumber);
 console.log(firstNumber * secondNumber);
@@ -29,11 +29,11 @@ console.log(score);
 score /= 4;
 console.log(score);
 
-const precedenceA = 2 + 3 * 4 - 5;
-const precedenceB = (2 + 3) * (4 - 5);
+let precedenceA = 2 + 3 * 4 - 5;
+let precedenceB = (2 + 3) * (4 - 5);
 console.log(precedenceA, precedenceB);
 
-const singleQuote = 'Single quotes';
-const doubleQuote = "Double quotes";
-const backtickString = `Template literal: ${singleQuote} & ${doubleQuote}`;
+let singleQuote = 'Single quotes';
+let doubleQuote = "Double quotes";
+let backtickString = `Template literal: ${singleQuote} & ${doubleQuote}`;
 console.log(singleQuote, doubleQuote, backtickString);
