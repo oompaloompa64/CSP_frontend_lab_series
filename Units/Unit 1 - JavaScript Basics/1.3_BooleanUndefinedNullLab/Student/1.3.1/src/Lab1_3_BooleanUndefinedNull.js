7dcd4709f546e572a707c10ce69c4b44827d3d2e
oompaloompa64 <nflores28150@columbushs.com>
2025-11-04 09:27:16 -0500

    finished 1.3.1

--- Units/Unit 1 - JavaScript Basics/1.3_BooleanUndefinedNullLab/Student/1.3.1/src/Lab1_3_BooleanUndefinedNull.js
+++ Units/Unit 1 - JavaScript Basics/1.3_BooleanUndefinedNullLab/Student/1.3.1/src/Lab1_3_BooleanUndefinedNull.js
@@ -5,22 +5,43 @@
 // TODO 1: Booleans
 // - Create isStudent = true, hasLicense = false
 // - Print results of true || false, true && false, !true
+let isStudent = true;
+let hasLicense = false;
+
+console.log(true || false); // true
+console.log(true && false); // false
+console.log(!true);         // false
 
 // TODO 2: Short-circuit evaluation
 // - Print result of false && (5 > 2)
 // - Print result of true || (2 > 5)
+console.log(false && (5 > 2)); // false
+console.log(true || (2 > 5));  // true
 
 // TODO 3: Undefined
 // - Declare variable grade and print it
+let grade;
+console.log(grade); // undefined
 
 // TODO 4: Null
 // - Create city = null, print it
 // - Reassign city = "Miami" and print again
+let city = null;
+console.log(city); // null
+
+city = "Miami";
+console.log(city); // "Miami"
 
 // TODO 5: Immutability
 // - Create word = "hello"
 // - Try word[0] = "H", print word
 // - Reassign word = "Hello" and print again
+let word = "hello";
+word[0] = "H";
+console.log(word); // "hello"
+
+word = "Hello";
+console.log(word); // "Hello"
 
 // 🚀 Stretch Goals
 // - Check if number is between 10 and 20 using &&
