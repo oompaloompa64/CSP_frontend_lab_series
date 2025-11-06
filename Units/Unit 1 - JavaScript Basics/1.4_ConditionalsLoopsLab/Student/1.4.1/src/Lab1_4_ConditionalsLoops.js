67cf2d89f99e5c9ba7accfa9530eac4ac4f1d823
oompaloompa64 <nflores28150@columbushs.com>
2025-11-06 14:01:12 -0500

    started 1.4.1

--- Units/Unit 1 - JavaScript Basics/1.4_ConditionalsLoopsLab/Student/1.4.1/src/Lab1_4_ConditionalsLoops.js
+++ Units/Unit 1 - JavaScript Basics/1.4_ConditionalsLoopsLab/Student/1.4.1/src/Lab1_4_ConditionalsLoops.js
@@ -4,9 +4,91 @@
 
 // TODO 1: if..else with age discounts
 
+let age = 15 //prompt("Enter your age:");
+if (age <= 16) {
+    console.log("teen discount");
+} else if (age >= 65) {
+    console.log("senior discount");
+} else {
+    console.log("no discount");
+}
+
+vpoints = 1200;
+let cart = 245
+let total;
+let discount;
+if(vpoints >= 5000){
+    total = cart * 0.8;
+    discount = "20%";
+} else if(vpoints >= 2000){
+    total = cart * 0.9;
+    discount = "10%";
+} else if(vpoints >= 1000){
+    total = cart * 0.95;
+    discount = "5%";
+} else{
+    total = cart;
+    discount = "0%";
+}
+console.log("Total: $" + total.toFixed(2) + " (Discount: " + discount + ")");
 // TODO 2: Nested if with two numbers
 
+let number1 = 6;
+let number2 = 7;
+
+if(number1 == number2){
+    console.log("Numbers are equal")
+} else if (number1 > number2){
+    console.log("number 1 is greater")
+} else {
+    console.log("number 1 is smaller")
+}
+
+
 // TODO 3: Switch statement for language greeting
+
+let language = "Spanish";
+
+switch(language){
+    case "spanish":
+        console.log("hola mundo")
+    break;
+    case "french":
+        console.log("bonjour tout le monde")
+    break;  
+    default:
+        console.log("hello world")
+    break;
+}
+
+let model = prompt("enter model of car");
+
+switch (model){
+    case "Focus":
+    case "F150":
+    case "Mustang":
+        console.log(`your ${model} is a Ford`)
+        break;
+    case "S":
+    case "3":
+    case "X":
+    case "Y":
+        console.log(`your ${model} is a Tesla`)
+        break;
+    case "911":
+    case "Carrara":
+    case "Tycan":
+        console.log(`your ${model} is a Porsche`)
+        break;
+    case "R8":
+    case "A4":
+    case "S4 cuatro":
+        console.log(`your ${model} is an audi`)
+        break;
+    default:
+        console.log("idk twin")
+
+}
 
 // TODO 4: While loop (1 to 10)
 
