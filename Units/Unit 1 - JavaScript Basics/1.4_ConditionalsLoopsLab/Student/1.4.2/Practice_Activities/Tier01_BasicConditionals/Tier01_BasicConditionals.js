0b780fcfb388ad6cb9e4113ba8f939f749ca2554
oompaloompa64 <nflores28150@columbushs.com>
2025-11-12 10:21:34 -0500

    started project

--- Units/Unit 1 - JavaScript Basics/1.4_ConditionalsLoopsLab/Student/1.4.2/Practice_Activities/Tier01_BasicConditionals/Tier01_BasicConditionals.js
+++ Units/Unit 1 - JavaScript Basics/1.4_ConditionalsLoopsLab/Student/1.4.2/Practice_Activities/Tier01_BasicConditionals/Tier01_BasicConditionals.js
@@ -1,2 +1,33 @@
 // Your code goes here. Follow the steps in README.md and run from index.html.
 console.log("TEST - TIER 1")
+
+let grade = Number(prompt("what it your grade?"));
+
+switch (true) {
+  case grade >= 90:
+    console.log("A")
+    break;
+  case grade >= 80:
+    console.log("B")
+    break;
+  case grade >= 70:
+    console.log("C")
+    break;
+  case grade >= 60:
+    console.log("D")
+    break;
+    case grade >= 0:
+    console.log("F")
+    break;
+    default:
+    console.log("WTF")
+}
+
+let age = Number(prompt("what is your age?"));
+
+if (age >= 18) {
+    console.log("You are eligable to vote.");
+}
+else {
+    console.log("You are not eligable to vote.");
+}
--- Units/Unit 1 - JavaScript Basics/1.4_ConditionalsLoopsLab/Student/1.4.2/Practice_Activities/Tier02_Loops/Tier02_Loops.js
+++ Units/Unit 1 - JavaScript Basics/1.4_ConditionalsLoopsLab/Student/1.4.2/Practice_Activities/Tier02_Loops/Tier02_Loops.js
@@ -1,2 +1,14 @@
 // Your code goes here. Follow the steps in README.md and run from index.html.
 console.log("TEST - TIER 2")
+
+for (let i = 1; i <= 10; i++) {
+    console.log(i);
+}
+
+let num = 10;
+
+while (num > 0) {
+    console.log(num);
+    num--;
+}
+console.log("Liftoff!");
